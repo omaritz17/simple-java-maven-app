b46251c7e92ba2f637672642806f7aa6909e5a41
omaritz17 <omarramtkd17@gmail.com>
2025-01-31 10:37:33 -0600

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -7,12 +7,12 @@ set +x
 
 echo 'Extracting the project name...'
 set -x
-NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | tr -d '[:space:]')
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed 's/[^a-zA-Z0-9._-]//g')
 set +x
 
 echo 'Extracting the project version...'
 set -x
-VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | tr -d '[:space:]')
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed 's/[^a-zA-Z0-9._-]//g')
 set +x
 
 JAR_FILE="target/${NAME}-${VERSION}.jar"
